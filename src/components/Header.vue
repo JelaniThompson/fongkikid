3b5b679f6b0b7b352379c675236156ac8cada8bc
Jelani Thompson <jelanithompson@Jelanis-MacBook-Pro-415.local>
2019-11-17 13:53:27 -0500

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="header">
+    <div class="header__name">
+      ENNA KIM <span class="header__fongki">[fongkikid]</span>
+    </div>
+    <div class="header__menuOptions">
+      <div class="header__projects header__tab">PROJECTS</div>
+      <div class="header__cv header__tab">CV</div>
+      <div class="header__contact header__tab">CONTACT</div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  top: 0;
+  width: 100%;
+  border: 5px solid black;
+  &__name {
+    justify-content: flex-start;
+    width: 45%;
+    font-weight: bold;
+    font-size: 36px;
+  }
+  // &__fongki {
+  //   // font-size: 24px;
+  //   // align-content: center;
+  //   // align-items: center;
+  //   // align-self: center;
+  // }
+  &__menuOptions {
+    width: 95%;
+    display: inline-flex;
+    justify-content: flex-end;
+  }
+  &__tab {
+    padding: 12px 16px;
+    justify-content: center;
+    position: relative;
+    border: 2px solid black;
+  }
+  &__projects {
+    border-left: 5px solid black;
+  }
+}
+</style>
---
+++ src/components/Homepage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="homepage">
+    <!-- Have header at the top -->
+    <Header />
+    <div class="homepage__main-section">
+      <img src="../assets/fongkikid.jpg" class="homepage__fongkikid-homepage" />
+      <p class="homepage__paragraph">
+        Enna Kim [fongkikid] is an interdisciplinary artist, designer and
+        researcher based in Toronto, Canada. <br /><br />
+        For my thesis, I am creating a speculative narrative animation that
+        explores how diasporic people choose to represent themselves in online
+        and offline spaces in order to further my understanding of my South
+        Korean roots.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./Header";
+export default {
+  name: "Homepage",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="scss">
+.homepage {
+  &__main-section {
+    display: flex;
+  }
+  &__fongkikid-homepage {
+    justify-content: flex-start;
+    border: 5px solid black;
+  }
+  &__paragraph {
+    margin-left: 2.5%;
+    margin-top: 15.5%;
+    max-width: 25%;
+    text-align: left;
+    font-weight: bold;
+  }
+}
+</style>
