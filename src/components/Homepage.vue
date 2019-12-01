fe93cd1352db669bc761259f648e2ebecfb4d77a
Jelani Thompson <jelanithompson@Jelanis-MacBook-Pro-415.local>
2019-12-01 16:59:48 -0500

    Homepage link styling

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -24,6 +24,28 @@
           allowfullscreen
         ></iframe>
       </div>
+      <p class="homepage__videoParagraph">
+        My thesis is a speculative narrative animation that talks about how
+        diasporic people navigate through their online and offline spaces. This
+        animation is a collection of in-person interviews, survey answers and
+        personal experiences that represent my childhood growing up as a
+        second-generation immigrant. I am using speculative narrative animation
+        as a design tool to further my understanding of my South Korean
+        identity. My methodology is a mixed approach, combining narrative
+        inquiry to design my survey (<span class="homepage__linkText"
+          ><a
+            href="https://forms.gle/zHqCmgV38zkKjJ4D7"
+            target="__blank"
+            class="homepage__videoParagraphLink"
+            >https://forms.gle/zHqCmgV38zkKjJ4D7</a
+          ></span
+        >), participatory action research for facilitating workshops and
+        speaking events. Online presence plays a strong role in my thesis
+        because the internet started to become my third parents. My project
+        visualizes the difficulties and discrepancies of being born between two
+        cultures and feeling the pressure to identify with one or the other
+        dependent on my surroundings.
+      </p>
     </div>
   </div>
 </template>
@@ -69,6 +91,17 @@ export default {
     // margin-top: 5%;
     padding-bottom: 5%;
   }
+  &__videoParagraph {
+    margin: 0 auto;
+    text-align: left;
+    width: 80%;
+    font-weight: bold;
+    font-size: 24px;
+  }
+  &__videoParagraphLink {
+    color: blue !important;
+  }
+
   // &__borderTwo {
   //   margin-top: 10%;
   //   padding-bottom: 10%;
